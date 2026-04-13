da94c69c39ecf0893fe833f5840ac3182b016e2a
Mykyta Pantelei <69463681+Fallka0@users.noreply.github.com>
2026-04-13 13:29:13 +0200

    fix: simplify next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {
-    root: __dirname,
-  },
-}
+  /* config options here */
+};
 
-export default nextConfig
+export default nextConfig;
